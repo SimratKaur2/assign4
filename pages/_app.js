6a56dd8de7a2318312a0b74700c8a8f9b48babaa
Simrat Kaur <simratkaur2@my.bcit.ca>
2023-05-20 17:30:17 -0700

    made the main page working!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function getActiveTheme(themeMode) {
+  return themeMode === "light" ? lightTheme : darkTheme;
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  const [activeTheme, setActiveTheme] = useState(lightTheme);
+  const [selectedTheme, setSelectedTheme] = useState("light");
+
+  const toggleTheme = () => {
+    const desiredTheme = selectedTheme === "light" ? "dark" : "light";
+
+    setSelectedTheme(desiredTheme);
+  };
+
+  useEffect(() => {
+    setActiveTheme(getActiveTheme(selectedTheme));
+  }, [selectedTheme]);
+
+  return (
+    <ThemeProvider theme={activeTheme}>
+      <Component {...pageProps} toggleTheme={toggleTheme} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
