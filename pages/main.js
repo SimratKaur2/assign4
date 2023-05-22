437603edae775a0d71a58509b846bf96f80180bd
Simrat Kaur <simratkaur2@my.bcit.ca>
2023-05-22 15:41:25 -0700

    got the matching logic to work on the main page.

--- pages/main.js
+++ pages/main.js
@@ -6,6 +6,8 @@ import DifficultyButtons from "../components/DifficultyButtons";
 import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Snackbar from "@mui/material/Snackbar";
+import Slide from "@mui/material/Slide";
 
 export default function Main({ toggleTheme }) {
   //state variables come here
@@ -22,22 +24,34 @@ export default function Main({ toggleTheme }) {
   const [timeRemaining, setTimeRemaining] = useState(totalTime);
   const [flippedCards, setFlippedCards] = useState([]);
 
+  const [pokemonCards, setPokemonCards] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const [guessingCards, setGuessingCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [isGuessing, setIsGuessing] = useState(false);
+  const [powerupActivated, setPowerupActivated] = useState(false);
+  const [isGameWon, setIsGameWon] = useState(false);
+
   //to render cards based off the difficuly selected
   const difficultyConfig = {
     easy: {
       level: 1,
       totalTime: 100,
       totalPairs: 4,
+      pairsLeft: 4,
     },
     medium: {
       level: 2,
       totalTime: 150,
       totalPairs: 6,
+      pairsLeft: 6,
     },
     hard: {
       level: 3,
       totalTime: 200,
       totalPairs: 8,
+      pairsLeft: 8,
     },
   };
 
@@ -54,7 +68,137 @@ export default function Main({ toggleTheme }) {
     }
   }, [timeRemaining, gameStarted]);
 
+  useEffect(() => {
+    if (selectedDifficulty) {
+      // Fetch totalPairs different Pokemon
+      const { totalPairs } = difficultyConfig[selectedDifficulty];
+      const fetchedPokemon = Array.from({ length: totalPairs }, () =>
+        fetchRandomPokemon()
+      );
+
+      // Duplicate each Pokemon and shuffle the array
+      const shuffledPokemon = shuffle([...fetchedPokemon, ...fetchedPokemon]);
+
+      setPokemonCards(shuffledPokemon);
+    }
+  }, [selectedDifficulty]);
+
+  useEffect(() => {
+    if (gameStarted) {
+      const fetchPokemon = async () => {
+        let pokemonArray = [];
+        const availablePokemonIds = Array.from(
+          { length: 810 },
+          (_, i) => i + 1
+        );
+
+        for (let i = 0; i < totalPairs; i++) {
+          const randomIndex = Math.floor(
+            Math.random() * availablePokemonIds.length
+          );
+          const randomPokemonId = availablePokemonIds.splice(randomIndex, 1)[0];
+          const pokemon = await getPokemon(randomPokemonId);
+
+          // Push two of each Pokemon into the array for matching pairs
+          pokemonArray.push(pokemon, pokemon);
+        }
+
+        // Shuffle the array so Pokemon appear at random positions
+        pokemonArray.sort(() => Math.random() - 0.5);
+
+        setPokemonData(pokemonArray);
+      };
+
+      fetchPokemon();
+    }
+  }, [gameStarted, totalPairs]);
+
+  useEffect(() => {
+    // If there are two cards being guessed and they don't match, flip them back over after a delay
+    if (
+      guessingCards.length === 2 &&
+      pokemonData[guessingCards[0]].id !== pokemonData[guessingCards[1]].id
+    ) {
+      setTimeout(() => {
+        setFlippedCards(
+          flippedCards.map((flipped, i) =>
+            guessingCards.includes(i) ? !flipped : flipped
+          )
+        );
+        setGuessingCards([]);
+      }, 1000);
+    }
+  }, [guessingCards, flippedCards, pokemonData]);
   //functions here //
+  //pokemon.sprites.front_default
+  // Function to fetch a random Pokemon
+  const fetchRandomPokemon = () => {
+    const randomId = Math.floor(Math.random() * 810) + 1;
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomId}.png`;
+  };
+
+  useEffect(() => {
+    if (pairsMatched === totalPairs) {
+      setIsGameWon(true);
+    }
+  }, [pairsMatched, totalPairs]);
+
+  useEffect(() => {
+    const powerupInterval = setInterval(() => {
+      //   alert("Powerup activated!");
+      if (!isGameWon) {
+        setPowerupActivated(true);
+        setFlippedCards((prevFlippedCards) =>
+          prevFlippedCards.map((flipped, index) =>
+            matchedCards.includes(index) ? flipped : !flipped
+          )
+        );
+
+        setTimeout(() => {
+          setFlippedCards((prevFlippedCards) =>
+            prevFlippedCards.map((flipped, index) =>
+              matchedCards.includes(index) ? flipped : !flipped
+            )
+          );
+        }, 2000);
+      }
+    }, 20000);
+
+    return () => {
+      clearInterval(powerupInterval);
+    };
+  }, [isGameWon]);
+
+  const getPokemon = async (id) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    const pokemon = {
+      id: data.id,
+      image: data.sprites.front_default,
+    };
+    return pokemon;
+  };
+
+  // Function to shuffle an array
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
 
   //1. to handle the Difficulty Buttons
   const handleDifficultyClick = (difficulty) => {
@@ -65,11 +209,13 @@ export default function Main({ toggleTheme }) {
   //3. logic to handle the starting game again
   const handleStartGameClick = () => {
     setGameStarted(true);
-    const { totalTime, totalPairs } = difficultyConfig[selectedDifficulty];
+    const { totalTime, totalPairs, pairsLeft } =
+      difficultyConfig[selectedDifficulty];
+    setPairsLeft(pairsLeft);
     setTimeRemaining(totalTime);
     setTotalPairs(totalPairs);
     setTotalTime(totalTime);
-
+    setIsGameWon(false);
     // initializing flippedCards based on totalPairs
     setFlippedCards(Array(totalPairs * 2).fill(true)); // Initialize flippedCards based on totalPairs
 
@@ -88,18 +234,74 @@ export default function Main({ toggleTheme }) {
     setTotalPairs(null);
     setTimeRemaining(null);
     setFlippedCards([]);
+    setGuessingCards([]);
+    setMatchedCards([]);
+    setIsGameWon(false);
+
     // Additional logic specific to resetting the game
     // Reset the timer, reshuffle the cards, etc.
   };
 
-  //5. logic to handle the card click
+  //   //5. logic to handle the card click
+  //   const handleCardClick = (index) => {
+  //     // Handle card click logic
+  //     setFlippedCards(
+  //       flippedCards.map((flipped, i) => (i === index ? !flipped : flipped))
+  //     );
+
+  //     // Increment click count, check for card matches, update pairs left/matched, etc.
+  //     setClickCount((prevCount) => prevCount + 1);
+  //   };
+
   const handleCardClick = (index) => {
-    // Handle card click logic
+    // If the card is already flipped or if we're in the middle of a guess, ignore the click
+    if (!flippedCards[index] || isGuessing || guessingCards.includes(index)) {
+      return;
+    }
+
+    // Flip the clicked card
     setFlippedCards(
       flippedCards.map((flipped, i) => (i === index ? !flipped : flipped))
     );
 
-    // Increment click count, check for card matches, update pairs left/matched, etc.
+    // If there are no cards currently being guessed, add this card to the guessingCards array
+    if (guessingCards.length === 0) {
+      setGuessingCards([index]);
+    }
+    // If there's one card currently being guessed, add this card to the guessingCards array and check if they match
+    else if (guessingCards.length === 1) {
+      setGuessingCards((oldGuessingCards) => [...oldGuessingCards, index]);
+
+      if (pokemonData[guessingCards[0]].id === pokemonData[index].id) {
+        // If they match, add them to the matchedCards array and remove them from the guessingCards array
+        setMatchedCards((oldMatchedCards) => [
+          ...oldMatchedCards,
+          ...guessingCards,
+          index,
+        ]);
+        setGuessingCards([]);
+        setPairsMatched(pairsMatched + 1);
+        setPairsLeft(pairsLeft - 1);
+      } else {
+        // If they don't match, flip them back over after a delay
+        // If they don't match, flip them back over after a delay
+        const currentGuessingCards = [...guessingCards, index];
+        setGuessingCards([]);
+
+        setIsGuessing(true); // Disable additional card flips
+
+        setTimeout(() => {
+          setFlippedCards((prevFlippedCards) =>
+            prevFlippedCards.map((flipped, i) =>
+              currentGuessingCards.includes(i) ? !flipped : flipped
+            )
+          );
+          setIsGuessing(false); // Enable card flips again
+        }, 1000);
+      }
+    }
+
+    // Increment click count
     setClickCount((prevCount) => prevCount + 1);
   };
 
@@ -114,6 +316,31 @@ export default function Main({ toggleTheme }) {
     if (gameStarted && selectedDifficulty) {
       const { level, totalTime, totalPairs } =
         difficultyConfig[selectedDifficulty];
+
+      if (isGameWon) {
+        return (
+          <Container
+            maxWidth={false}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h4" color={theme.palette.text.primary} mb={2}>
+              Congratulations! You've won the game!
+            </Typography>
+            <Button
+              variant="outlined"
+              size="large"
+              onClick={handleResetGameClick}
+            >
+              Play Again!
+            </Button>
+          </Container>
+        );
+      }
 
       return (
         <Container
@@ -175,9 +402,7 @@ export default function Main({ toggleTheme }) {
                     }}
                   >
                     <img
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                        Math.floor(Math.random() * 810) + 1
-                      }.png`}
+                      src={pokemonData[index]?.image}
                       alt={`Pokemon ${index + 1}`}
                       style={{
                         width: "80%",
@@ -255,6 +480,28 @@ export default function Main({ toggleTheme }) {
         justifyContent="center"
         alignItems="center"
       >
+        <Snackbar
+          open={powerupActivated}
+          message="Powerup activated!"
+          autoHideDuration={3000}
+          onClose={() => setPowerupActivated(false)}
+          sx={{
+            transformOrigin: {
+              vertical: "top",
+              horizontal: "right",
+            },
+            transform: "translateX(-50%)",
+          }}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+          TransitionComponent={Slide}
+          TransitionProps={{
+            direction: "left",
+          }}
+        />
+
         <Typography variant="h2" color={theme.palette.text.primary}>
           Pokémon Memory Match
         </Typography>
